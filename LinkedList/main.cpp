0d90c46b9af67d949ddc5a1280b534b217047820
joemichaletz <joemichaletz@gmail.com>
2020-02-11 10:06:47 -0600

    First Commit

---
+++ LinkedList/main.cpp
@@ -0,0 +1,11 @@
+
+#include "LinkedList.hpp"
+
+int main()
+{
+    LinkedList* ll = new LinkedList(); //some change
+    ll->addFront(1);
+    ll->addFront(2);
+    ll->display();
+    delete ll;
+}
